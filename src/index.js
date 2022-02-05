2f759f94b22d8a23f2b322d67a7c37c356cef2f7
Leyla Becker <git@jan-leila.com>
2022-02-04 21:20:00 -0600

    created get pronouns

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 
-function get_pronouns(){
-
+let code_map = [
+    [ 'she', 'her' ],
+    [ 'he', 'him' ],
+    [ 'they', 'them' ],
+    [ 'any', 'all' ],
+]
+function pronoun_builder(code, index){
+    return code_map[code][Math.min(index, 1)];
 }
 
-function get_code(){
+// get sub codes from code
+function get_sub_codes(code){
+    let codes = [ code % 3, undefined, undefined];
+    codes[1] = code >= 3 ? (codes[0] + Math.floor(code / 3) % 2 + 1) % 3 : codes[0];
+    codes[2] = code >= 9 ? (codes[0] * 2 - codes[1] + 3) % 3 : codes[1];
+    return codes.filter((c, i, arr) => arr.indexOf(c) === i);
+}
+
+// get pronoun string from code
+function get_pronouns(code){
+    if(code == undefined){
+        return undefined;
+    }
+    let codes = code === 15? [ 3 ]:get_sub_codes(code);
+    codes = [ ...codes, codes[0] ].slice(0, Math.max(codes.length, 2));
+    return codes.map(pronoun_builder).join('/');
+}
+
+// get code from pronoun string
+function get_code(pronouns){
 
 }
 
